c95ece9823c5581fa89073b700e247259aa4c6e0
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-07 13:50:19 -0400

    Broke out the main function from the page itself

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,84 +1,9 @@
+import 'package:app/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'package:app/services/api_service.dart';
-import 'package:app/services/device_service.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Home()
   ),
   );
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() {
-    return _HomeState();
-  }
-}
-
-class _HomeState extends State<Home> {
-  late String? _deviceId;
-  late String? _userId;
-  //late List<Unit> _units = null;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _deviceId = _userId = null;
-    _initializeApp();
-  }
-
-  void _initializeApp() async {
-    _deviceId = (await DeviceService().getId())!;
-    _userId = (await ApiService().getUserId(_deviceId!));
-    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar (
-        title: const Text('API Test'),
-      ),
-      body: _userId == null || _userId!.isEmpty
-        ? const Center(
-          child: CircularProgressIndicator(),
-          )
-        : ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column (
-                children: [
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('User ID: $_userId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('Device ID: $_deviceId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  )
-                ],
-              )
-            );
-          }
-        )
-    );
-  }
 }
