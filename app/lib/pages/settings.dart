f4ad7854b0af24118cb1aaec261c6c677abc4b0a
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-11 23:59:16 -0400

    Added a test for autoformatting text to a hyphenated guid and showing/removing elements based on global variable

--- app/lib/pages/settings.dart
+++ app/lib/pages/settings.dart
@@ -1,75 +1,157 @@
 import 'package:flutter/material.dart';
-import 'package:app/services/device_service.dart';
-
+import 'package:flutter/services.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
-
   @override
+  // ignore: library_private_types_in_public_api
   _SettingsState createState() => _SettingsState();
 }
 
 class _SettingsState extends State<Settings> {
-  late String? _deviceId;
-  late String? _userId;
-  //late List<Unit> _units = null;
+  bool _enabled = false;
 
   @override
   void initState() {
     super.initState();
 
-    _deviceId = _userId = null;
+    _enabled = false;
     _initializeApp();
   }
 
   void _initializeApp() async {
-    _deviceId = (await DeviceService().getId())!;
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar (
+    return Scaffold(
+      appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: _userId == null || _userId!.isEmpty
-        ? const Center(
-          child: CircularProgressIndicator(),
-          )
-        : ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column (
-                children: [
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('User ID: $_userId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('Device ID: $_deviceId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
+      body: Column(
+        children: <Widget>[
+          if (!_enabled) ...[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Enable Sharing Units",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Column(children: [
+                  Switch(
+                    value: _enabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _enabled = value;
+                      });
+                    },
                   )
-                ],
-              )
-            );
-          }
-        )
+                ])
+              ],
+            ),
+          ] else ...[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Enable Sharing Units",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Column(children: [
+                  Switch(
+                    value: _enabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _enabled = value;
+                      });
+                    },
+                  )
+                ])
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                    ),
+                    inputFormatters: [
+                      GuidInputFormatter()
+                    ],
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    style: TextStyle(fontSize: 20),
+                    maxLines: 1,
+                    maxLength: 36,
+                    keyboardType: TextInputType.text,
+                    enabled: true,
+                  ),
+                )
+              ],
+            ),
+          ]
+        ],
+      ),
     );
   }
+}
+
+class GuidInputFormatter extends TextInputFormatter {
+  static const separator = '-';
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+      if(newValue.text.isEmpty) {
+        return newValue.copyWith(text: '');
+      }
+
+      String oldValueText = oldValue.text.replaceAll(separator, '');
+      String newValueText = newValue.text.replaceAll(separator, '');
+
+      if(oldValue.text.endsWith(separator) && oldValue.text.length == newValue.text.length + 1) {
+        newValueText = newValueText.substring(0, newValueText.length - 1);
+      }
+
+      if(oldValueText != newValueText) {
+        int selectionIndex = newValue.text.length - newValue.selection.extentOffset;
+
+        String newString = '';
+        switch(oldValue.text.length){
+          case 8:
+          case 13:
+          case 18:
+          case 23:
+            newString = '${newValue.text}-';
+            break;
+          default:
+            newString = newValue.text;
+            break;
+        }
+
+        return TextEditingValue(
+          text: newString.toString(),
+          selection: TextSelection.collapsed(
+            offset: newString.length - selectionIndex
+          )
+        );
+      }
+
+      return newValue;
+    }
 }
