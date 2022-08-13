ccecf25af1c092beecd2bf3bf5466b6f80a50532
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-12 21:05:46 -0400

    Setup basic layout of the settings page

--- app/lib/pages/settings.dart
+++ app/lib/pages/settings.dart
@@ -11,6 +11,8 @@ class Settings extends StatefulWidget {
 
 class _SettingsState extends State<Settings> {
   bool _enabled = false;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _groupIdController = TextEditingController();
 
   @override
   void initState() {
@@ -20,9 +22,19 @@ class _SettingsState extends State<Settings> {
     _initializeApp();
   }
 
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _groupIdController.dispose();
+
+    super.dispose();
+  }
+
   void _initializeApp() async {
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
+
+  void onPressed() {}
 
   @override
   Widget build(BuildContext context) {
@@ -33,80 +45,194 @@ class _SettingsState extends State<Settings> {
       body: Column(
         children: <Widget>[
           if (!_enabled) ...[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Enable Sharing Units",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                Column(children: [
-                  Switch(
-                    value: _enabled,
-                    onChanged: (value) {
-                      setState(() {
-                        _enabled = value;
-                      });
-                    },
-                  )
-                ])
-              ],
-            ),
-          ] else ...[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Enable Sharing Units",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                Column(children: [
-                  Switch(
-                    value: _enabled,
-                    onChanged: (value) {
-                      setState(() {
-                        _enabled = value;
-                      });
-                    },
-                  )
-                ])
-              ],
+            SizedBox(
+              height: 10.0,
+              child: Container(),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Expanded(
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  // height: 80.0,
                   child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                    decoration: const InputDecoration(
+                      // border: OutlineInpuUtBorder(),
+                      hintText: 'Username',
+                      hintStyle: TextStyle(fontSize: 19),
                     ),
-                    inputFormatters: [
-                      GuidInputFormatter()
-                    ],
+                    // inputFormatters: [
+                    //   GuidInputFormatter()
+                    // ],
                     textAlign: TextAlign.center,
                     textAlignVertical: TextAlignVertical.center,
-                    style: TextStyle(fontSize: 20),
+                    style: const TextStyle(fontSize: 19),
                     maxLines: 1,
-                    maxLength: 36,
+                    maxLength: 100,
                     keyboardType: TextInputType.text,
                     enabled: true,
+                    controller: _usernameController,
                   ),
                 )
               ],
             ),
+            SizedBox(
+              child: Row(
+                children: [
+                  SizedBox(
+                    // height: 80.0,
+                    width: MediaQuery.of(context).size.width * 0.07,
+                    child: Container(),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Enable Sharing Units",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: Container()),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Switch(
+                          value: _enabled,
+                          onChanged: (value) {
+                            setState(() {
+                              _enabled = value;
+                            });
+                          },
+                        )
+                      ])
+                ],
+              ),
+            ),
+          ] else ...[
+            SizedBox(
+              height: 10.0,
+              child: Container(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  // height: 80.0,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      // border: OutlineInpuUtBorder(),
+                      hintText: 'Username',
+                      hintStyle: TextStyle(fontSize: 19),
+                    ),
+                    // inputFormatters: [
+                    //   GuidInputFormatter()
+                    // ],
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    style: const TextStyle(fontSize: 19),
+                    maxLines: 1,
+                    maxLength: 100,
+                    keyboardType: TextInputType.text,
+                    enabled: true,
+                    controller: _usernameController,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Row(
+                children: [
+                  SizedBox(
+                    // height: 80.0,
+                    width: MediaQuery.of(context).size.width * 0.02,
+                    child: Container(),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Enable Sharing Units",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.26,
+                      child: Container()),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Switch(
+                          value: _enabled,
+                          onChanged: (value) {
+                            setState(() {
+                              _enabled = value;
+                            });
+                          },
+                        )
+                      ])
+                ],
+              ),
+            ),
+                            SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: 20.0,
+                  child: Container()
+                ),
+                        Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  // height: 80.0,
+                  child: const Text(
+                  'Share Value for Other Devices to Join Your Group or Update to Join Someone Elses Group',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.visible,
+                  softWrap: true,
+                  style: TextStyle(fontSize: 12)
+                )
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  // height: 80.0,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                      hintStyle: TextStyle(fontSize: 19),
+                    ),
+                    inputFormatters: [GuidInputFormatter()],
+                    textAlign: TextAlign.center,
+                    textAlignVertical: TextAlignVertical.center,
+                    style: const TextStyle(fontSize: 17),
+                    maxLines: 1,
+                    maxLength: 36,
+                    keyboardType: TextInputType.text,
+                    enabled: true,
+                    controller: _groupIdController,
+                  ),
+                ),
+              ],
+            ),
           ]
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Save Settings',
+        child: const Icon(Icons.save),
       ),
     );
   }
@@ -116,42 +242,29 @@ class GuidInputFormatter extends TextInputFormatter {
   static const separator = '-';
 
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-      if(newValue.text.isEmpty) {
-        return newValue.copyWith(text: '');
-      }
-
-      String oldValueText = oldValue.text.replaceAll(separator, '');
-      String newValueText = newValue.text.replaceAll(separator, '');
-
-      if(oldValue.text.endsWith(separator) && oldValue.text.length == newValue.text.length + 1) {
-        newValueText = newValueText.substring(0, newValueText.length - 1);
-      }
-
-      if(oldValueText != newValueText) {
-        int selectionIndex = newValue.text.length - newValue.selection.extentOffset;
-
-        String newString = '';
-        switch(oldValue.text.length){
-          case 8:
-          case 13:
-          case 18:
-          case 23:
-            newString = '${newValue.text}-';
-            break;
-          default:
-            newString = newValue.text;
-            break;
-        }
-
-        return TextEditingValue(
-          text: newString.toString(),
-          selection: TextSelection.collapsed(
-            offset: newString.length - selectionIndex
-          )
-        );
-      }
-
-      return newValue;
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue.copyWith(text: '');
     }
+
+    String newString = '';
+    String tempString = newValue.text.replaceAll(separator, '');
+
+    for (var i = 0; i < tempString.length; i++) {
+      newString += tempString[i];
+
+      if (i == 7 || i == 11 || i == 15 || i == 19) {
+        newString += separator;
+      }
+    }
+
+    if (newString[newString.length - 1] == separator) {
+      newString = newString.substring(0, newString.length - 1);
+    }
+
+    return TextEditingValue(
+        text: newString.toString(),
+        selection: TextSelection.collapsed(offset: newString.length));
+  }
 }
