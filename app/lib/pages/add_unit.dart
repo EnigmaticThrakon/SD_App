0a93a16b7fbc57d3825a43bdd42441566c79c3ca
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-15 22:38:11 -0400

    Created the basic layout and logic for adding units

--- app/lib/pages/add_unit.dart
+++ app/lib/pages/add_unit.dart
@@ -1,30 +1,35 @@
+import 'package:app/models/group.dart';
+import 'package:app/models/unit.dart';
+import 'package:app/services/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:app/services/device_service.dart';
 
 class AddUnit extends StatefulWidget {
-  const AddUnit({Key? key}) : super(key: key);
+  AddUnit({Key? key, required this.apiService, required this.group}) : super(key: key);
 
+  ApiService apiService;
+  Group group;
 
   @override
-  // ignore: library_private_types_in_public_api
-  _AddUnitState createState() => _AddUnitState();
+  // ignore: library_private_types_in_public_api, no_logic_in_create_state
+  _AddUnitState createState() => _AddUnitState(apiService, group);
 }
 
 class _AddUnitState extends State<AddUnit> {
-  late String? _deviceId;
-  late String? _userId;
-  //late List<Unit> _units = null;
+  _AddUnitState(this._apiService, this.group);
+
+  final ApiService _apiService;
+  Group group;
+  List<Unit> _availableUnits = <Unit>[];
 
   @override
   void initState() {
     super.initState();
 
-    _deviceId = _userId = null;
     _initializeApp();
   }
 
   void _initializeApp() async {
-    _deviceId = (await DeviceService().getId())!;
+    _availableUnits = (await _apiService.getAvailableUnits(group));
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
 
@@ -34,42 +39,51 @@ class _AddUnitState extends State<AddUnit> {
       appBar: AppBar (
         title: const Text('Add Unit'),
       ),
-      body: _userId == null || _userId!.isEmpty
-        ? const Center(
-          child: CircularProgressIndicator(),
-          )
-        : ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column (
-                children: [
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('User ID: $_userId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row (
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('Device ID: $_deviceId'),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
+      body: _availableUnits.isEmpty ? const Center(
+              child: CircularProgressIndicator(),
+            ) : ListView.builder(
+              itemCount: _availableUnits.length + 1,
+              itemBuilder: (context, index) {
+                return index != 0 ? GestureDetector(
+                  onTap: () => {
+                    _availableUnits[index - 1].selected != null ? _availableUnits[index - 1].selected = !_availableUnits[index - 1].selected! : true,
+                    setState(() => {})
+                  },
+                  child: Card(
+                    child: 
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            _availableUnits[index - 1].id!,
+                            style: const TextStyle(fontSize: 16)
+                            ),
+                          Checkbox(
+                            value: _availableUnits[index - 1].selected != null ? _availableUnits[index - 1].selected! : false, 
+                            onChanged: (value) => {
+                              _availableUnits[index - 1].selected = value,
+                              setState(() => {})
+                            })
+                          ]
+                      )
                   )
-                ],
-              )
-            );
-          }
-        ),
+                ) : const Padding(
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: Text(
+                  "Select the Devices to Add",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                  )
+                );
+              }
+            ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {  
+          },
+        tooltip: 'Save Selected Units',
+        child: const Icon(Icons.save),
+      ),
     );
   }
 }
