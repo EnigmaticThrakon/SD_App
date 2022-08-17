169c370b8bf7bcec7b0693e577f0c53c42f2b26c
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-17 19:55:33 -0400

    Added observables and routing for when a new unit connects

--- app/lib/services/signalr_service.dart
+++ app/lib/services/signalr_service.dart
@@ -1,16 +1,31 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:signalr_client/signalr_client.dart';
+import 'package:mobx/mobx.dart';
+
+import '../models/unit.dart';
 
 class SignalRService {
-  static String baseUrl = 'http://paranoidandroid.network:52042/appMonitorHub';
-  late HubConnection hubConnection;
+  final String _baseUrl = 'http://paranoidandroid.network:52042/appMonitorHub';
+  late HubConnection _hubConnection;
+  final Observable<Unit> _onNewUnit = Observable<Unit>(Unit());
 
-  Future<void> connect() async {
+  Future<void> connect(String userId) async {
     try {
-      hubConnection = HubConnectionBuilder().withUrl(baseUrl).build();
-      hubConnection.start();
+      _hubConnection = HubConnectionBuilder().withUrl('$_baseUrl?user=$userId').build();
+      _hubConnection.start();
+
+      _hubConnection.on("newConnection", (value) => {
+        _onNewUnit.value = Unit.fromJson(value[0] as Map<String, dynamic>),
+        _onNewUnit.reportChanged()
+      });
+
     } catch (e) {
       log(e.toString());
     }
+  }
+
+  Observable getOnNewUnit() {
+    return _onNewUnit;
   }
 }
