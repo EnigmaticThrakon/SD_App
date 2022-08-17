43de59dccb9f5015e0eb2ffd99dd2abe7c437689
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-17 19:59:11 -0400

    Added paired ID to model

--- app/lib/models/unit.dart
+++ app/lib/models/unit.dart
@@ -3,22 +3,26 @@ class Unit {
   String? publicIP;
   bool? selected;
   bool? isConnected;
+  String? pairedId;
 
   Unit({
     this.id,
     this.publicIP,
     this.selected,
-    this.isConnected
+    this.isConnected,
+    this.pairedId
   });
   
   factory Unit.fromJson(Map<String, dynamic> json) => Unit(
     id: json["id"],
     publicIP: json["publicIP"],
-    isConnected: json["isConnected"]
+    isConnected: json["isConnected"],
+    pairedId: json["pairedId"]
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
-    "publicIP": publicIP
+    "publicIP": publicIP,
+    "pairedId": pairedId
   };
 }
