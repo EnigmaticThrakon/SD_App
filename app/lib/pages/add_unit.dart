2bd9c87543291e140d416e1375c6e14ccf8d5c79
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-17 21:33:57 -0400

    Added subscriber for when a unit connects on the adding unit page

--- app/lib/pages/add_unit.dart
+++ app/lib/pages/add_unit.dart
@@ -1,23 +1,29 @@
 import 'package:app/models/group.dart';
 import 'package:app/models/unit.dart';
 import 'package:app/services/api_service.dart';
+import 'package:app/services/signalr_service.dart';
 import 'package:flutter/material.dart';
 
 class AddUnit extends StatefulWidget {
-  AddUnit({Key? key, required this.apiService, required this.group}) : super(key: key);
+  const AddUnit({Key? key, required this.apiService, required this.signalR, required this.group, required this.publicIP}) : super(key: key);
 
-  ApiService apiService;
-  Group group;
+  final ApiService apiService;
+  final Group group;
+  final SignalRService signalR;
+  final String publicIP;
 
   @override
   // ignore: library_private_types_in_public_api, no_logic_in_create_state
-  _AddUnitState createState() => _AddUnitState(apiService, group);
+  _AddUnitState createState() => _AddUnitState(apiService, signalR, group, publicIP);
 }
 
 class _AddUnitState extends State<AddUnit> {
-  _AddUnitState(this._apiService, this.group);
+  _AddUnitState(this._apiService, this._signalRService, this.group, this._publicIP);
 
   final ApiService _apiService;
+  final SignalRService _signalRService;
+  final String _publicIP;
+
   Group group;
   List<Unit> _availableUnits = <Unit>[];
 
@@ -30,7 +36,27 @@ class _AddUnitState extends State<AddUnit> {
 
   void _initializeApp() async {
     _availableUnits = (await _apiService.getAvailableUnits(group));
+
+    setupSubscribers();
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+  }
+
+  void setupSubscribers() async {
+    Unit tempUnit;
+    _signalRService.getOnNewUnit().observe((onNewUnit) => {
+      tempUnit = (onNewUnit.newValue as Unit),
+
+      if(tempUnit.isConnected! && 
+         tempUnit.publicIP != null &&
+         tempUnit.publicIP! == _publicIP &&
+         (tempUnit.pairedId == null ||
+          tempUnit.pairedId == "00000000-0000-0000-0000-000000000000")) {
+
+          _availableUnits.add(tempUnit)
+         },
+
+      setState(() => {})
+    });
   }
 
   @override
