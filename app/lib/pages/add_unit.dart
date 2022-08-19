48fcd1143f4d61c14a0b926baf3524eeee882f6f
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-19 18:44:07 -0400

    Added logic to save button to update server

--- app/lib/pages/add_unit.dart
+++ app/lib/pages/add_unit.dart
@@ -5,7 +5,13 @@ import 'package:app/services/signalr_service.dart';
 import 'package:flutter/material.dart';
 
 class AddUnit extends StatefulWidget {
-  const AddUnit({Key? key, required this.apiService, required this.signalR, required this.group, required this.publicIP}) : super(key: key);
+  const AddUnit(
+      {Key? key,
+      required this.apiService,
+      required this.signalR,
+      required this.group,
+      required this.publicIP})
+      : super(key: key);
 
   final ApiService apiService;
   final Group group;
@@ -14,11 +20,13 @@ class AddUnit extends StatefulWidget {
 
   @override
   // ignore: library_private_types_in_public_api, no_logic_in_create_state
-  _AddUnitState createState() => _AddUnitState(apiService, signalR, group, publicIP);
+  _AddUnitState createState() =>
+      _AddUnitState(apiService, signalR, group, publicIP);
 }
 
 class _AddUnitState extends State<AddUnit> {
-  _AddUnitState(this._apiService, this._signalRService, this.group, this._publicIP);
+  _AddUnitState(
+      this._apiService, this._signalRService, this.group, this._publicIP);
 
   final ApiService _apiService;
   final SignalRService _signalRService;
@@ -44,69 +52,75 @@ class _AddUnitState extends State<AddUnit> {
   void setupSubscribers() async {
     Unit tempUnit;
     _signalRService.getOnNewUnit().observe((onNewUnit) => {
-      tempUnit = (onNewUnit.newValue as Unit),
-
-      if(tempUnit.isConnected! && 
-         tempUnit.publicIP != null &&
-         tempUnit.publicIP! == _publicIP &&
-         (tempUnit.pairedId == null ||
-          tempUnit.pairedId == "00000000-0000-0000-0000-000000000000")) {
-
-          _availableUnits.add(tempUnit)
-         },
-
-      setState(() => {})
-    });
+          tempUnit = (onNewUnit.newValue as Unit),
+          if (tempUnit.isConnected! &&
+              tempUnit.publicIP != null &&
+              tempUnit.publicIP! == _publicIP &&
+              (tempUnit.pairedId == null ||
+                  tempUnit.pairedId == "00000000-0000-0000-0000-000000000000"))
+            {_availableUnits.add(tempUnit)},
+          setState(() => {})
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar (
+    return Scaffold(
+      appBar: AppBar(
         title: const Text('Add Unit'),
       ),
-      body: _availableUnits.isEmpty ? const Center(
+      body: _availableUnits.isEmpty
+          ? const Center(
               child: CircularProgressIndicator(),
-            ) : ListView.builder(
-              itemCount: _availableUnits.length + 1,
+            )
+          : ListView.builder(
+              itemCount: _availableUnits.length,
               itemBuilder: (context, index) {
-                return index != 0 ? GestureDetector(
-                  onTap: () => {
-                    _availableUnits[index - 1].selected != null ? _availableUnits[index - 1].selected = !_availableUnits[index - 1].selected! : true,
-                    setState(() => {})
-                  },
-                  child: Card(
-                    child: 
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            _availableUnits[index - 1].id!,
-                            style: const TextStyle(fontSize: 16)
-                            ),
-                          Checkbox(
-                            value: _availableUnits[index - 1].selected != null ? _availableUnits[index - 1].selected! : false, 
-                            onChanged: (value) => {
-                              _availableUnits[index - 1].selected = value,
+                return index != 0
+                    ? GestureDetector(
+                        onTap: () => {
+                              _availableUnits[index].selected != null
+                                  ? _availableUnits[index].selected =
+                                      !_availableUnits[index].selected!
+                                  : true,
                               setState(() => {})
-                            })
-                          ]
-                      )
-                  )
-                ) : const Padding(
-                  padding: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Text(
-                  "Select the Devices to Add",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18),
-                  )
-                );
-              }
-            ),
+                            },
+                        child: Card(
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                              Text(_availableUnits[index].id!,
+                                  style: const TextStyle(fontSize: 16)),
+                              Checkbox(
+                                  value: _availableUnits[index].selected != null
+                                      ? _availableUnits[index].selected!
+                                      : false,
+                                  onChanged: (value) => {
+                                        _availableUnits[index].selected = value,
+                                        setState(() => {})
+                                      })
+                            ])))
+                    : const Padding(
+                        padding: EdgeInsets.only(top: 20, bottom: 10),
+                        child: Text(
+                          "Select the Devices to Add",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18),
+                        ));
+              }),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {  
-          },
+        onPressed: () async {
+          for (var i = 1; i < _availableUnits.length; i++) {
+            if (_availableUnits[i].selected == true) {
+              _availableUnits[i].pairedId = group.userId;
+              _apiService.linkUnit(_availableUnits[i]);
+            }
+          }
+
+          Navigator.pop(context);
+        },
         tooltip: 'Save Selected Units',
         child: const Icon(Icons.save),
       ),
