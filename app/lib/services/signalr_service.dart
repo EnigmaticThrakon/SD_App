b9514191dddaa759dff6ebc6f00c630930b37925
EnigmaticThrakon <gbrown92799@gmail.com>
2022-08-19 18:45:31 -0400

    Added listener for when a device is linked

--- app/lib/services/signalr_service.dart
+++ app/lib/services/signalr_service.dart
@@ -8,24 +8,39 @@ import '../models/unit.dart';
 class SignalRService {
   final String _baseUrl = 'http://paranoidandroid.network:52042/appMonitorHub';
   late HubConnection _hubConnection;
+
   final Observable<Unit> _onNewUnit = Observable<Unit>(Unit());
+  final Observable<Unit> _onLinkedUnit = Observable<Unit>(Unit());
 
   Future<void> connect(String deviceId) async {
     try {
       _hubConnection = HubConnectionBuilder().withUrl('$_baseUrl?deviceId=$deviceId').build();
       _hubConnection.start();
 
-      _hubConnection.on("newConnection", (value) => {
-        _onNewUnit.value = Unit.fromJson(value[0] as Map<String, dynamic>),
-        _onNewUnit.reportChanged()
-      });
+      setupListeners(_hubConnection);
 
     } catch (e) {
       log(e.toString());
     }
   }
 
+  void setupListeners(HubConnection connection) {
+      connection.on("newConnection", (value) => {
+        _onNewUnit.value = Unit.fromJson(value[0] as Map<String, dynamic>),
+        _onNewUnit.reportChanged()
+      });
+
+      connection.on("unitLinked", (value) => {
+        _onLinkedUnit.value = Unit.fromJson(value[0] as Map<String, dynamic>),
+        _onLinkedUnit.reportChanged()
+      });
+  }
+
   Observable getOnNewUnit() {
     return _onNewUnit;
+  }
+
+  Observable getOnLinkedUnit() {
+    return _onLinkedUnit;
   }
 }
