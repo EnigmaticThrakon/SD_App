53fa452d9cbf045f253510caa990cb2273eefa88
EnigmaticThrakon <gbrown92799@gmail.com>
2022-10-30 14:01:54 -0400

    Added a chart and subscription to read in live data

--- app/lib/pages/unit_detailed.dart
+++ app/lib/pages/unit_detailed.dart
@@ -1,6 +1,8 @@
 import 'package:app/services/api_service.dart';
 import 'package:app/services/signalr_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mrx_charts/mrx_charts.dart';
 
 import '../models/unit.dart';
 
@@ -29,8 +31,55 @@ class _UnitDetailedState extends State<UnitDetailed> {
   final ApiService _apiService;
   final SignalRService _signalRService;
   final TextEditingController _deviceNameController = TextEditingController();
-
+  ChartLineDataItem currentValue = ChartLineDataItem(x: DateTime.now().millisecondsSinceEpoch.toDouble(), value: 0.0);
+  List<ChartLineDataItem> chartValues = <ChartLineDataItem>[ChartLineDataItem(value: 0.0, x: DateTime.now().millisecondsSinceEpoch.toDouble())];
+  List<double> tempValues = <double>[];
+  bool isAcquisitioning = false;
   bool settingsChanged = false;
+  double maxValue = 0;
+  double minValue = 0;
+
+  List<ChartLayer> layers() {
+    final from = chartValues.first.x;
+    final to = chartValues.last.x;
+    final frequency =
+        (to - from) / 3.0;
+    return [
+      ChartAxisLayer(
+        settings: ChartAxisSettings(
+          x: ChartAxisSettingsAxis(
+            frequency: (chartValues.last.x - chartValues.first.x == chartValues.last.x ? chartValues.last.x - 1 : chartValues.first.x) / 2,
+            max: chartValues.last.x,
+            min: chartValues.first.x,
+            textStyle: const TextStyle(
+              color: Colors.black,
+              fontSize: 10.0,
+            ),
+          ),
+          y: ChartAxisSettingsAxis(
+            frequency: maxValue - minValue,
+            max: maxValue,
+            min: minValue,
+            textStyle: const TextStyle(
+              color: Colors.black,
+              fontSize: 10.0,
+            ),
+          ),
+        ),
+        labelX: (value) => DateFormat('mm:ss')
+            .format(DateTime.fromMillisecondsSinceEpoch(value.toInt())),
+        // labelX: (value) => "",
+        labelY: (value) => value.toString(),
+      ),
+      ChartLineLayer(
+        items: chartValues,
+        settings: const ChartLineSettings(
+          color: Colors.black,
+          thickness: 1.0,
+        ),
+      ),
+    ];
+  }
 
   @override
   void initState() {
@@ -51,7 +100,27 @@ class _UnitDetailedState extends State<UnitDetailed> {
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
 
-  void onPressed() {}
+  void setupSubscribers() {
+      _signalRService.getOnChangeValue().observe((value) => {
+          setState(() => {
+            tempValues.add(value.newValue),
+            currentValue = ChartLineDataItem(value: value.newValue, x: DateTime.now().millisecondsSinceEpoch.toDouble()),
+
+            if(tempValues.length > 5) {
+              chartValues.add(ChartLineDataItem(x: DateTime.now().millisecondsSinceEpoch.toDouble(), value: tempValues.reduce((value, element) => value = value + element) / tempValues.length)),
+              tempValues.clear()
+            },
+
+            if(currentValue.value > maxValue) {
+              maxValue = currentValue.value,
+            },
+
+            if(currentValue.value < minValue) {
+              minValue = currentValue.value
+            }
+          })
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +129,12 @@ class _UnitDetailedState extends State<UnitDetailed> {
         title: const Text('Unit Details',
             style: TextStyle(fontFamily: 'Serif', fontWeight: FontWeight.bold)),
       ),
-      body: Column(children: <Widget>[
+      body: 
+      //  chartValues == null
+      //     ? const Center(
+      //         child: CircularProgressIndicator(),
+      //       )
+        Column(children: <Widget>[
         SizedBox(
           height: 10.0,
           child: Container(),
@@ -94,12 +168,47 @@ class _UnitDetailedState extends State<UnitDetailed> {
         ),
         SizedBox(
           width: MediaQuery.of(context).size.width * 0.7,
+          child: Text(currentValue.value.toString())
+        ),
+        Container(
+          constraints: BoxConstraints(
+            maxHeight: 200,
+            maxWidth: MediaQuery.of(context).size.width,
+          ),
+          // padding: const EdgeInsets.all(24.0),
+          child: Chart(
+            layers: layers(),
+            // padding: const EdgeInsets.symmetric(horizontal: 30.0).copyWith(
+            //   bottom: 12.0,
+            // ),
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: ElevatedButton(
+              onPressed: () async => {
+                    setupSubscribers(),
+                    isAcquisitioning = !isAcquisitioning,
+                    if(!isAcquisitioning) {
+                      await _apiService.stopAcquisitioning(_unit.id!),
+                      setState(() => {})
+                    } else {
+                      await _apiService.startAcquisition(_unit.id!)
+                    },
+                  },
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(isAcquisitioning ? Colors.red : Colors.green)),
+              child: Text(isAcquisitioning ? "Stop Acquisitioning" : "Start Acquisition",
+                style: const TextStyle(fontSize: 20, fontFamily: 'Serif'),
+              )),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
           child: ElevatedButton(
               onPressed: () async => {
                     await _apiService.removeUnitFromUser(_unit.id!, ""),
                     Navigator.pop(context)
                   },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
               child: const Text(
                 "Unpair Unit",
                 style: TextStyle(fontSize: 20, fontFamily: 'Serif'),
