bdc7cee1032ab44e9ae3a020ef47985dfef44f21
EnigmaticThrakon <gbrown92799@gmail.com>
2022-11-10 22:07:40 -0500

    Added more form fields and the ability to save

--- app/lib/pages/unit_settings.dart
+++ app/lib/pages/unit_settings.dart
@@ -1,8 +1,10 @@
 import 'package:app/services/api_service.dart';
 import 'package:flutter/material.dart';
+import '../models/unit_settings.dart';
 
-class UnitSettings extends StatefulWidget {
-  const UnitSettings({Key? key, required this.apiService, required this.unitId})
+class UnitSettingsPage extends StatefulWidget {
+  const UnitSettingsPage(
+      {Key? key, required this.apiService, required this.unitId})
       : super(key: key);
 
   final ApiService apiService;
@@ -10,17 +12,20 @@ class UnitSettings extends StatefulWidget {
 
   @override
   // ignore: library_private_types_in_public_api
-  _UnitSettingsState createState() =>
+  _UnitSettingsPageState createState() =>
       // ignore: no_logic_in_create_state
-      _UnitSettingsState(apiService, unitId);
+      _UnitSettingsPageState(apiService, unitId);
 }
 
-class _UnitSettingsState extends State<UnitSettings> {
-  _UnitSettingsState(this._apiService, this._unitId);
+class _UnitSettingsPageState extends State<UnitSettingsPage> {
+  _UnitSettingsPageState(this._apiService, this._unitId);
 
   final String _unitId;
   final ApiService _apiService;
-  final TextEditingController _serialNumberController = TextEditingController();
+  final TextEditingController _unitNameController = TextEditingController();
+  final TextEditingController _humidityController = TextEditingController();
+  final TextEditingController _temperatureController = TextEditingController();
+  final TextEditingController _airFlowController = TextEditingController();
 
   @override
   void initState() {
@@ -30,81 +35,214 @@ class _UnitSettingsState extends State<UnitSettings> {
   }
 
   void _initializeApp() async {
+    unitSettings = await _apiService.getUnitSettings(_unitId);
+
+    _unitNameController.text = unitSettings.name!;
+
+    if(unitSettings.unitParameters != null) {
+      if(unitSettings.unitParameters!.airFlow != null) {
+        _airFlowController.text = unitSettings.unitParameters!.airFlow.toString();
+      }
+
+      if(unitSettings.unitParameters!.temperature != null) {
+        _temperatureController.text = unitSettings.unitParameters!.temperature.toString();
+      }
+
+      if(unitSettings.unitParameters!.humidity != null) {
+        _humidityController.text = unitSettings.unitParameters!.humidity.toString();
+      }
+    }
+
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
 
+  late UnitSettings unitSettings = UnitSettings();
   final _unitSettingsKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Unit Settings',
-            style: TextStyle(fontFamily: 'Serif', fontWeight: FontWeight.bold)),
-      ),
-      body: Form(
-        key: _unitSettingsKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Device Name'
-                ),
-              validator: (value) {
-                if(value == null || value.isEmpty) {
-                  return 'Name Cannot Be Empty';
-                }
-
-                return null;
-              },
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontFamily: 'Serif'
-              )
-            )),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row (
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Humidity'),
-                    Padding(
-                padding: const EdgeInsets.all(10),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.4,
-                  child:                TextFormField(
-                  validator: (value) {
-                    if(value == null || value.isEmpty) {
-                      return 'Humidity Cannot Be Empty';
-                    }
-
-                    return null;
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-                )
-              ),
-
-                  ]
-                )
-              ),
-            ElevatedButton(
-              onPressed: () {
-                if(_unitSettingsKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-              child: const Text('Submit')
-            )
-          ]
+        appBar: AppBar(
+          title: const Text('Unit Settings',
+              style:
+                  TextStyle(fontFamily: 'Serif', fontWeight: FontWeight.bold)),
         ),
-      )
-    );
+        body: unitSettings.id == null
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+              child:Form(
+                key: _unitSettingsKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: TextFormField(
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Device Name'),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return '';
+                                }
+
+                                return null;
+                              },
+                              textAlign: TextAlign.center,
+                              controller: _unitNameController,
+                              style: const TextStyle(fontFamily: 'Serif'))),
+                      Padding(
+                          padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: const [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 10, 0, 27),
+                                        child: Text('Humidity: ',
+                                            style: TextStyle(
+                                                fontFamily: 'Serif',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20)))
+                                  ]),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: const [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 27, 0, 27),
+                                        child: Text('Air Flow: ',
+                                            style: TextStyle(
+                                                fontFamily: 'Serif',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20)))
+                                  ]),
+                                  Row(children: const [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 27, 0, 15),
+                                        child: Text('Temperature: ',
+                                            style: TextStyle(
+                                                fontFamily: 'Serif',
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20)))
+                                  ]),
+                                ]),
+                                Column(children: [
+                                  Row(children: [
+                                    Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 10, 0, 10),
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.2,
+                                          child: TextFormField(
+                                            decoration: const InputDecoration(
+                                                border: OutlineInputBorder()),
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return '';
+                                              }
+
+                                              return null;
+                                            },
+                                            textAlign: TextAlign.center,
+                                            controller: _humidityController,
+                                            keyboardType: TextInputType.number,
+                                          ),
+                                        ))
+                                  ]),
+                                  Row(children: [
+                                    Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 10, 0, 10),
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.2,
+                                          child: TextFormField(
+                                            decoration: const InputDecoration(
+                                                border: OutlineInputBorder()),
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return '';
+                                              }
+
+                                              return null;
+                                            },
+                                            textAlign: TextAlign.center,
+                                            controller: _airFlowController,
+                                            keyboardType: TextInputType.number,
+                                          ),
+                                        ))
+                                  ]),
+                                  Row(children: [
+                                    Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 10, 0, 10),
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.2,
+                                          child: TextFormField(
+                                            decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                ),
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return '';
+                                              }
+
+                                              return null;
+                                            },
+                                            textAlign: TextAlign.center,
+                                            controller: _temperatureController,
+                                            keyboardType: TextInputType.number,
+                                          ),
+                                        ))
+                                  ]),
+                                ])
+                              ])),
+                      ElevatedButton(
+                          onPressed: () async {
+                            if (_unitSettingsKey.currentState!.validate()) {
+                              unitSettings.name = _unitNameController.value.text;
+                              unitSettings.unitParameters!.humidity = double.parse(_humidityController.value.text);
+                              unitSettings.unitParameters!.temperature = double.parse(_temperatureController.value.text);
+                              unitSettings.unitParameters!.airFlow = double.parse(_airFlowController.value.text);
+                              var response = await _apiService.saveUnitSettings(unitSettings);
+
+                              if(response == 0) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text('Settings Saved')),
+                                );
+                                Navigator.pop(context, unitSettings.name);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text('Save Failed')),
+                                );
+                              }
+                            }
+                          },
+                          child: const Text('Save',
+                            style: TextStyle(
+                              fontFamily: 'Serif',
+                              fontSize: 20
+                            )))
+                    ]),
+              )));
   }
 }
