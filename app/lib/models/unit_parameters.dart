af2d51e5a29715b71ced8509b336de45ac5cde71
EnigmaticThrakon <gbrown92799@gmail.com>
2022-11-10 22:06:04 -0500

    Updated converting to and from json

--- app/lib/models/unit_parameters.dart
+++ app/lib/models/unit_parameters.dart
@@ -14,10 +14,18 @@ class UnitParameters {
   });
   
   factory UnitParameters.fromJson(Map<String, dynamic> json) => UnitParameters(
-    temperature: json["Temperature"],
-    humidity: json["Humidity"],
-    airFlow: json["AirFlow"],
-    weight: json["Weight"],
-    door: json["Door"]
+    temperature: double.parse(json["temperature"].toString()),
+    humidity: double.parse(json["humidity"].toString()),
+    airFlow: double.parse(json["airFlow"].toString()),
+    weight: double.parse(json["weight"].toString()),
+    door: json["door"]
   );
+
+  Map<String, dynamic> toJson() => {
+    "temperature": temperature,
+    "humidity": humidity,
+    "airFlow": airFlow,
+    "weight": weight,
+    "door": door
+  };
 }
